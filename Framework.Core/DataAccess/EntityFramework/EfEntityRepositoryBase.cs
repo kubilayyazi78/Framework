7a5de06968c888d3211dbf397108d8c23f5999a5
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-23 21:04:59 +0300

    Entity framework implementasyonu yapıldı.

---
+++ Framework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,39 @@
+﻿using Framework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity> where TEntity : class, IEntity, new() where TContext : DbContext, new()
+    {
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> File = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> File = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
