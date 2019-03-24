8d823ac70ca6c9941c41ffab532eb7db8d88c4e4
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-24 03:57:15 +0300

    NHibernate implementasyonu yapıldı.

---
+++ Framework.Core/DataAccess/NHibabernate/NhQueryableRepository.cs
@@ -0,0 +1,33 @@
+﻿using Framework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Core.DataAccess.NHibabernate
+{
+    public class NhQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+        IQueryable<T> _entities;
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table { get { return this.Entites; } }
+
+        public virtual IQueryable<T> Entites
+        {
+            get
+            {
+                if (_entities == null)
+                {
+                    _entities = _nHibernateHelper.OpenSession().Query<T>();
+                }
+                return _entities;
+            }
+        }
+    }
+}
