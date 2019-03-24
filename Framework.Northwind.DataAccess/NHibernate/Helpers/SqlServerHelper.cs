aadde94507f4d09d16333f2b6047569a2544cfec
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-24 17:43:01 +0300

    Nhibernate sqlserverhelper implementasyonu yapıldı.

---
+++ Framework.Northwind.DataAccess/NHibernate/Helpers/SqlServerHelper.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Framework.Core.DataAccess.NHibabernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Northwind.DataAccess.NHibernate.Helpers
+{
+    public class SqlServerHelper : NHibernateHelper
+    {
+        protected override global::NHibernate.ISessionFactory InitializeFactory()
+        {
+            return Fluently.Configure().Database(MsSqlConfiguration.MsSql7.ConnectionString(c=>c.FromConnectionStringWithKey("NorthwindContext"))).Mappings(t=>t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())).BuildSessionFactory();
+        }
+    }
+}
