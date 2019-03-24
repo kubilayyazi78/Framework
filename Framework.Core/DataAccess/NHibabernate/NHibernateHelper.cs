9133e42d5323f60a857c6ecf9565e2eb644249d8
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-24 03:38:59 +0300

    NHibernateHelper oluşturuldu.

---
+++ Framework.Core/DataAccess/NHibabernate/NHibernateHelper.cs
@@ -0,0 +1,37 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Core.DataAccess.NHibabernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+        static ISessionFactory _sessionFactory;
+
+        public  virtual ISessionFactory SessionFactory
+        {
+            get
+            {
+                return _sessionFactory ?? (_sessionFactory = InitializeFactory());
+            }
+        }
+        protected abstract ISessionFactory InitializeFactory();
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        //private ISessionFactory InitializeFactory()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
