78e1c88b612366857a36e6f7d04e0ea12b7e5096
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-24 18:04:37 +0300

    Nhibernate mappin işlemi yapıldı.

---
+++ Framework.Northwind.DataAccess/NHibernate/Mappings/ProductMap.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Mapping;
+using Framework.Northwind.Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Northwind.DataAccess.NHibernate.Mappings
+{
+  public  class ProductMap:ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Table(@"Products");
+            LazyLoad();
+            Id(x => x.ProductId).Column("ProductID");
+
+            Map(x => x.CategoryId).Column("CategoryID");
+            Map(x => x.ProductName).Column("ProductName");
+            Map(x => x.QuantityPerUnit).Column("QuantityPerUnit");
+            Map(x => x.UnitPrice).Column("UnitPrice");
+
+        }
+    }
+}
